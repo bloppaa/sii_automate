070db4fc49173b7f8eeb14faffd0841afba6d632
Pablo <pablocortes1101@gmail.com>
2025-12-25 03:14:06 -0300

    funciona. implementar manejo de errores en el futuro

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const { chromium } = require("playwright");
+const PDFMerger = require("pdf-merger-js").default;
 const fs = require("fs");
+const path = require("path");
 require("dotenv").config();
+
+const merger = new PDFMerger();
+const documentsDir = path.join(__dirname, "documents");
+const outputFile = path.join(__dirname, "merged.pdf");
 
 const LOGIN_URL =
   "https://zeusr.sii.cl/AUT2000/InicioAutenticacion/IngresoRutClave.html?https://www1.sii.cl/cgi-bin/Portal001/mipeSelEmpresa.cgi?DESDE_DONDE_URL=OPCION%3D52%26TIPO%3D4";
-const HOME_URL =
-  "https://www1.sii.cl/cgi-bin/Portal001/mipeGenFacEx.cgi?PTDC_CODIGO=52";
+
 const DOCUMENT_LIST_URL = `https://www1.sii.cl/cgi-bin/Portal001/mipeAdminDocsEmi.cgi?ORDEN=&NUM_PAG=1&recaptcha-response=&RUT_RECP=&FOLIO=&RZN_SOC=&TPO_DOC=&ESTADO=`;
+const DOCUMENT_CODE_REGEX =
+  /\/cgi-bin\/Portal001\/mipeGesDocEmi\.cgi\?ALL_PAGE_ANT=2&CODIGO=(\d+)/;
+const DOCUMENT_URL =
+  "https://www1.sii.cl/cgi-bin/Portal001/mipeGenFacEx.cgi?IGUAL=CODIGO&PTDC_CODIGO=52&VALOR=";
 
 async function login(page) {
   await page.goto(LOGIN_URL);
@@ -15,17 +24,19 @@ async function login(page) {
   await page.getByRole("button", { name: "Ingresar", exact: true }).click();
 }
 
-async function listDocuments(page, copyDate) {
-  await page.waitForURL(HOME_URL);
+async function getDocumentsCodes(page, copyDate) {
+  await page.waitForLoadState();
   await page.goto(
     DOCUMENT_LIST_URL + `&FEC_DESDE=${copyDate}&FEC_HASTA=${copyDate}`
   );
-  const rows = await page.locator("#tablaDatos td.sorting_1 a").all();
-  const targets = rows.map((row) => row.());
+  const rows = await page.locator("td.sorting_1 a").all();
+  const urls = await Promise.all(rows.map((row) => row.getAttribute("href")));
+  return urls.map((url) => DOCUMENT_CODE_REGEX.exec(url)[1]);
 }
 
-async function copyDocument(page, targetDate) {
-  await page.getByRole("link", { name: "Copiar Documento" }).click();
+async function copyDocument(page, targetDate, documentCode) {
+  await page.goto(DOCUMENT_URL + documentCode);
+  await page.bringToFront();
   await page
     .locator('select[name="cbo_dia_boleta"]')
     .selectOption(targetDate.split("-")[2]);
@@ -47,7 +58,7 @@ async function signDocument(page) {
   await page.getByRole("button", { name: "Firmar" }).click();
 }
 
-async function downloadDocument(page) {
+async function downloadDocument(page, context, documentCode) {
   const [newPage] = await Promise.all([
     context.waitForEvent("page"),
     page.getByRole("link", { name: "Ver Documento" }).click(),
@@ -56,7 +67,7 @@ async function downloadDocument(page) {
   const pdfUrl = newPage.url();
   const response = await newPage.request.get(pdfUrl);
   const buffer = await response.body();
-  fs.writeFileSync("documento.pdf", buffer);
+  fs.writeFileSync(`documents/${documentCode}.pdf`, buffer);
 }
 
 (async () => {
@@ -65,11 +76,21 @@ async function downloadDocument(page) {
   const page = await context.newPage();
 
   await login(page);
-  await listDocuments(page, "2025-12-18");
-  await copyDocument(page, "2025-12-21");
-  // await signDocument(page);
-  // await downloadDocument(page);
-
-  await page.pause();
+  const codes = await getDocumentsCodes(page, "2025-12-23");
+  for (const code of codes) {
+    await copyDocument(page, "2025-12-07", code);
+    await signDocument(page);
+    await downloadDocument(page, context, code);
+  }
   await browser.close();
+
+  const files = fs.readdirSync(documentsDir);
+  const pdfFiles = files.filter((file) => file.toLowerCase().endsWith(".pdf"));
+
+  for (const file of pdfFiles) {
+    const filePath = path.join(documentsDir, file);
+    await merger.add(filePath);
+  }
+
+  await merger.save(outputFile);
 })();
